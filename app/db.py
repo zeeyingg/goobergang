056c551f210e001ef7874db92cafcf16b3b92098
Joshua Liu <jliu31@stuy.edu>
2023-05-04 14:07:32 -0400

    add

--- app/db.py
+++ app/db.py
@@ -1,3 +1,26 @@
 import sqlite3
 
 Database = "test.db"
+
+db = sqlite3.connect(Database, check_same_thread=False)
+c=db.cursor()
+c.executescript(
+    """
+    create TABLE if NOT EXISTS Lectures(Lecture_id int primary key)
+    """
+)
+c.close()
+
+def get_all_lecture_id():
+    c = db.cursor()
+    c.execute("select Lecture_id from Lectures")
+    data = c.fetchall()
+    c.close()
+    if(data == []):
+        return None
+    return data
+
+def add_lecture(Lecture_id):
+    c = db.cursor()
+    c.execute("insert into Lectures values()")
+    c.close()
