deb7c75055d915aaebf1298a99c4ac1701746df6
Joshua Liu <jliu31@stuy.edu>
2023-05-04 13:52:46 -0400

    added some starting files

---
+++ app/__init__.py
@@ -0,0 +1,21 @@
+'''
+goobergang: 
+'''
+
+from flask import *
+from db import *
+
+app = Flask(__name__)
+app.secret_key = "temporarykey" #maybe not needed since we dont have logins
+
+@app.route("/", methods=["GET"])
+def home_page():
+    return render_template("index.html")
+
+@app.route("/result", methods=["GET"])
+def result_page():
+    return render_template("result.html", searchterm=request.args.get("search"))
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
