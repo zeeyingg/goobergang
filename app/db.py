0b0e62d610df4876b4c13ee8fba47fc96d8f705b
Joshua Liu <jliu31@stuy.edu>
2023-05-08 14:05:55 -0400

    worked on the databse stuff

--- app/db.py
+++ app/db.py
@@ -6,7 +6,10 @@ db = sqlite3.connect(Database, check_same_thread=False)
 c=db.cursor()
 c.executescript(
     """
-    create TABLE if NOT EXISTS Lectures(Lecture_id int primary key, lecture_title text)
+    create TABLE if NOT EXISTS Lectures(Lecture_id int primary key, lecture_title text, professor_id int, topic text, speed int, vocabulary int);
+    create TABLE if NOT EXISTS Professor(professor_id primary key int, professor text, speed int, vocabulary int, sentiment int, stu_interact int);
+    create TABLE if NOT EXISTS Scripts(Lecture_id int, line_id int, line text, time text);
+    create TABLE if NOT EXISTS Subject(topic text, speed text, vocabulary text, sentiment text, stu_interact int);
     """
 )
 c.close()
@@ -22,7 +25,55 @@ def get_all_lecture_id():
 
 def add_lecture(Lecture_id, lecture_title):
     c = db.cursor()
-    c.execute("insert into Lectures values(?, ?)", (Lecture_id, lecture_title))
+    
+    c.execute("SELECT MAX(professor_id) FROM Lectures")
+    max_id = c.fetchone()
+    if (max_id[0] != None):
+        new_id = max_id[0] + 1
+    else:
+        new_id = 0
+    c.execute("insert into Lectures values(?, ?, ?)", (Lecture_id, lecture_title, new_id))
+    db.commit()
     c.close()
 
-# def get_all_lecture_name():
+def get_all_lecture_title(): # Might be useful for searching up lectures by name
+    c = db.cursor()
+    c.execute("select lecture_title from Lectures")
+    data = c.fetchall()
+    c.close()
+    if(data == []):
+        return None
+    return data
+
+def get_one_lecture_title(Lecture_id): # Maybe not so useful
+    c = db.cursor()
+    c.execute("select Lecture_title from Lectures where Lecture_id = ?", (Lecture_id))
+    data = c.fetchone()
+    c.close()
+    if(result == None):
+        return None
+    else: 
+        return result[0]
+
+# ==============================
+def get_all_professor_id(): # returns a list of all the professor_ids
+    c = db.cursor()
+    c.execute("select professor_id from Professor")
+    data = c.fetchall()
+    c.close()
+    if data == None : 
+        return []
+    IDs = [id[0] for id in data]
+    return IDs
+
+def get_professor_info(ID): # Gets professor Info by ID
+    c = db.cursor()
+    c.execute("")
+    data = c.fetchall()
+    c.close()
+
+# Not sure if we need this
+#def get_all_professor_info():
+
+#==============================
+add_lecture(-2,"test_name")
